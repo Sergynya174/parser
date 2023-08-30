745aeab7b72afce11e956509f47473d663e34a6f
Sergynya174 <Sergynya174@yandex.ru>
2023-08-30 12:52:39 +0300

    доработки

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const title = document.querySelectorAll(".title");
 const button = document.querySelector(".next-page");
 
 const arr = localStorage.getItem("arr");
-const newArr = JSON.parse(arr);
+const newArr = JSON.parse(arr) || [];
 
 title.forEach((item) => {
   let domainPattern =
     /(?:(?:https?|ftp):\/\/)?(?:www\.)?([a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]+)/g;
   let res = item.innerText.match(domainPattern);
   if (res) {
-    Array(newArr).push(res[0]);
+    newArr.push(res[0]);
   }
   localStorage.setItem("arr", JSON.stringify(newArr));
 });
